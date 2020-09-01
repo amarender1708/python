43ec14461e9dfa40bc58c41c58c4d35fba686e6d
Nagarajan <amarender.nagarajan@travelport.com>
2020-09-01 16:29:39 -0600

    git commit master

--- hello.py
+++ hello.py
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
+
 print("Hello World")
 dded a new line for hotfix
-=======
 This line is added in fix101
 print(Hello World)
 print("This is another hot fix")
->>>>>>> fix101
-<<<<<<< HEAD
-
-=======
 print("This is the fix for ticket 102")
 exit 0
->>>>>>> fix102
+
+
