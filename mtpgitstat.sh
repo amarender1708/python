0e030c152ff94da3145c71c6c1a62e7600a989b7
Nagarajan <amarender.nagarajan@travelport.com>
2020-09-06 12:30:02 -0600

    changes made to commit script

--- mtpgitstat.sh
+++ mtpgitstat.sh
@@ -1,8 +1,22 @@
-pushd /c/Amar/Personal/git/demo
-git status; _RC=$? &> /dev/null
-if ["$_RC" -eq 0 ]; then
-	echo "The directory is clean"
+pushd /c/Amar/Personal/git/demo &> /dev/null;
+git status; _RC=$?;
+if [ "$_RC" -eq 0 ]; then
+	
+	git add .; _RC=$?;
+	if [ "$_RC" -eq 0 ]; then
+		git commit -am "there is an updated code which is commited"; _RC=$?;
+		if [ "$_RC" -eq 0 ]; then
+			echo "Commit successfull"
+		else
+			echo "Commit unsuccesfull"
+		fi
+	else
+		echo "There is an issue with the git add"
+
+	fi
+
+	
 else
-	eco "SOme files are not commited"
+	echo "THis is not a git directory"
 
 fi
