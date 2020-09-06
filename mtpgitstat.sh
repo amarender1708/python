91e811388889bfb0b88d38ca81c3d4fb39032b75
amarender1708 <68969067+amarender1708@users.noreply.github.com>
2020-09-06 14:40:43 -0600

    commit change

--- mtpgitstat.sh
+++ mtpgitstat.sh
@@ -3,7 +3,7 @@ git status; _RC=$?;
 if [ "$_RC" -eq 0 ]; then	
 	git add .; _RC=$?;
 	if [ "$_RC" -eq 0 ]; then
-		git commit -am "there is an updated code which is commited"; _RC=$?;
+		git commit -am "There is an updated code which is commited"; _RC=$?;
 		if [ "$_RC" -eq 0 ]; then
 			echo "Commit was Successfull"
 		else
